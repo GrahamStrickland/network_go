edfd6f0d2e0ced00733c5bf2b3cb4ff6f895bfdb
Graham Strickland <grahamhstrickland@gmail.com>
2024-09-20 18:28:45 +0200

    Cut TestListenerAccept() method from listen_test.go to avoid infinite loop

--- src/ch03/listen_test.go
+++ src/ch03/listen_test.go
@@ -14,24 +14,3 @@ func TestListener(t *testing.T) {
 
 	t.Logf("bound to %q", listener.Addr())
 }
-
-func TestListenerAccept(t *testing.T) {
-	listener, err := net.Listen("tcp", "127.0.0.1:0")
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			t.Fatal(err)
-		}
-
-		go func(c net.Conn) {
-			defer c.Close()
-
-			t.Logf("conn: %v\n", conn)
-		}(conn)
-	}
-}
